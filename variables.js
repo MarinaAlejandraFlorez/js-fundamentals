370ad7eaf94bdd63a45b15165250cf82729c9fca
Tag134 <Taniaarango09@gmail.com>
2022-06-21 21:21:11 -0500

    review about differences between var and let

---
+++ variables.js
@@ -0,0 +1,30 @@
+console.log("Entrando")
+saludar()
+
+
+//Declaración de la variable
+var producto 
+
+//Inicialización de la variable
+var producto = "Perfumes"
+
+//Reasignar variable
+producto="cremas"
+
+//hoisting = elevación
+console.log (conHoisting)
+
+var conHoisting
+
+//scope = Alcance
+ function saludar() {
+    var saludo ="Hola desde función"
+    let saludoLocal="Y solo accedo desde mi scope"
+    //console.log(saludo) 
+ }
+
+ saludo="Hola de nuevo"
+ console.log(saludo)
+ console.log(saludoLocal)
+
+
